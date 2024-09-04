c121018c44a483d6c4821b4c57ef9611a6b25243
Jennifer Goldwin <jennifer.goldwin@binus.ac.id>
2024-09-05 01:24:21 +0800

    add withdrawal t id

--- rizq-nasional-website/src/main/java/com/restapi/rizqnasionalwebsite/entity/WithdrawalTransactionRequest.java
+++ rizq-nasional-website/src/main/java/com/restapi/rizqnasionalwebsite/entity/WithdrawalTransactionRequest.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class WithdrawalTransactionRequest {
+    private String id;
     private String userId;
     private String status;
 }
